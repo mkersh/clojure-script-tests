8e9d82f99c4ce4ddc41740971a18b0d299809a83
mkersh <Jamieson$87654321>
2020-10-18 16:59:56 +0100

    update

--- src/clojure_script_tests/core.cljs
+++ src/clojure_script_tests/core.cljs
@@ -4,7 +4,7 @@
 (enable-console-print!)
 
 (println "This text is printed from src/clojure-script-tests/core.cljs. Go ahead and edit it and see reloading in actionTTTT.")
-(println "Hello world255")
+(println "Hello world25566")
 
 ;; define your app data so that it doesn't get over-written on reload
 
